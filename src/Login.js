82d6369fe0cd9e5328669abca9d5cc593bfdc2cb
Reecha Kumari <reechakumari0810@gmail.com>
2021-04-17 22:48:57 +0530

    Form validation

---
+++ src/Login.js
@@ -0,0 +1,15 @@
+import React from  'react';
+import './App.css';
+
+export default function Login (){
+    return(
+        <div class="form">
+          <form class="login-form">
+            <input type="text" placeholder="username"/>
+            <input type="password" placeholder="password"/>
+            <button>login</button>
+            <p class="message">Not registered? <a href="#">Create an account</a></p>
+          </form>
+        </div>
+    );
+}
---
+++ src/Registration.js
@@ -0,0 +1,16 @@
+import React from  'react';
+import './App.css';
+
+export default function Registration (){
+    return(
+        <div class="form">
+          <form class="register-form">
+            <input type="text" placeholder="name"/>
+            <input type="password" placeholder="password"/>
+            <input type="text" placeholder="email address"/>
+            <button>create</button>
+            <p class="message">Already registered? <a href="#">Sign In</a></p>
+          </form>
+        </div>
+    );
+}
